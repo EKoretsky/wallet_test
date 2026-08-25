14375f7e551a4aed47e23f6a877000f311e4c7c6
Eugene Koretsky <E.Koretsky@outlook.com>
2026-08-26 01:32:16 +0300

    Improve address formatting and copy feedback

--- lib/features/address/address_tile.dart
+++ lib/features/address/address_tile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:wallet_test/core/theme/app_tokens.dart';
-
+import 'package:wallet_test/features/address/address_display.dart';
 import 'package:wallet_test/features/address/address_tile_bloc.dart';
 
 class AddressTile extends StatefulWidget {
@@ -29,6 +30,10 @@ class _AddressTileState extends State<AddressTile> {
 
   @override
   Widget build(BuildContext context) {
+    const addressFontSize = 14.0;
+    final scaledAddressFontSize = MediaQuery.textScalerOf(context).scale(addressFontSize);
+    final textScaleFactor = scaledAddressFontSize / addressFontSize;
+
     return Container(
       height: AppTokens.cellHeight,
       padding: const .symmetric(horizontal: AppTokens.horizontalPadding),
@@ -44,14 +49,15 @@ class _AddressTileState extends State<AddressTile> {
                 Text(
                   widget.network,
                   style: const TextStyle(
-                    fontSize: 12,
+                    fontSize: 12.0,
                     color: AppTokens.textSecondary,
                   ),
+                  overflow: .ellipsis,
                 ),
                 Text(
-                  widget.address,
+                  formatAddressForCell(widget.address, textScaleFactor),
                   style: const TextStyle(
-                    fontSize: 14,
+                    fontSize: addressFontSize,
                     color: AppTokens.textPrimary,
                   ),
                   overflow: .ellipsis,
@@ -59,16 +65,64 @@ class _AddressTileState extends State<AddressTile> {
               ],
             ),
           ),
-          IconButton(
-            onPressed: () => _bloc.add(CopyTapped(widget.address)),
-            icon: const Icon(
-              Icons.copy,
-              size: 20,
-              color: Colors.black87,
-            ),
+          const SizedBox(width: AppTokens.gapTextIcon),
+          BlocBuilder<AddressTileBloc, AddressTileState>(
+            bloc: _bloc,
+            builder: (context, state) {
+              final appearance = _CopyButtonAppearance.fromState(state);
+
+              return SizedBox.square(
+                dimension: AppTokens.tapTarget,
+                child: IconButton(
+                  padding: .zero,
+                  constraints: const BoxConstraints(),
+                  onPressed: () => _bloc.add(CopyTapped(widget.address)),
+                  icon: Icon(
+                    appearance.icon,
+                    size: appearance.iconSize,
+                    color: appearance.color,
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
     );
   }
+}
+
+class _CopyButtonAppearance {
+  const _CopyButtonAppearance({
+    required this.icon,
+    required this.color,
+    required this.iconSize
+  });
+
+  factory _CopyButtonAppearance.fromState(AddressTileState state) {
+    final (:icon, :color) = switch (state) {
+      AddressTileState(error: String _) => (
+        icon: Icons.error_outline,
+        color: AppTokens.danger,
+      ),
+      AddressTileState(copied: true) => (
+        icon: Icons.check,
+        color: AppTokens.success,
+      ),
+      _ => (
+        icon: Icons.copy,
+        color: AppTokens.textSecondary,
+      ),
+    };
+
+    return _CopyButtonAppearance(
+      icon: icon,
+      color: color,
+      iconSize: AppTokens.iconSize
+    );
+  }
+
+  final IconData icon;
+  final Color color;
+  final double iconSize;
 }
