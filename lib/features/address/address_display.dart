69f4a47a42a70e7d337ec41776e2c2e675e7c5d9
Eugene Koretsky <E.Koretsky@outlook.com>
2026-08-25 17:56:48 +0300

    Add responsive address formatting

---
+++ lib/features/address/address_display.dart
@@ -0,0 +1,27 @@
+String formatAddressForCell(String address, double textScaleFactor) {
+  final addressParts = address.splitHexPrefix();
+  final prefix = addressParts.prefix;
+
+  if (addressParts.body.length <= 12) {
+    return address;
+  }
+
+  final leadingCharacterCount = textScaleFactor < 1.6 ? 6 : 4;
+  final leading = addressParts.body.substring(0, leadingCharacterCount);
+  final trailing = addressParts.body.substring(addressParts.body.length - 4);
+  const placeholder = '...';
+
+  return '$prefix$leading$placeholder$trailing';
+}
+
+extension on String {
+  ({String prefix, String body}) splitHexPrefix() {
+    const prefix = 'Ox';
+    final hasPrefix = startsWith(prefix);
+
+    return (
+    prefix: hasPrefix ? substring(0, prefix.length) : '',
+    body: hasPrefix ? substring(prefix.length) : this,
+    );
+  }
+}
