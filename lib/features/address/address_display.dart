0377c6729514e9e2f05031dd2474066a2c7a84fd
Eugene Koretsky <E.Koretsky@outlook.com>
2026-08-25 18:43:38 +0300

    Fix hexadecimal address formatting

--- lib/features/address/address_display.dart
+++ lib/features/address/address_display.dart
@@ -9,19 +9,19 @@ String formatAddressForCell(String address, double textScaleFactor) {
   final leadingCharacterCount = textScaleFactor < 1.6 ? 6 : 4;
   final leading = addressParts.body.substring(0, leadingCharacterCount);
   final trailing = addressParts.body.substring(addressParts.body.length - 4);
-  const placeholder = '...';
+  const placeholder = '…';
 
   return '$prefix$leading$placeholder$trailing';
 }
 
 extension on String {
   ({String prefix, String body}) splitHexPrefix() {
-    const prefix = 'Ox';
+    const prefix = '0x';
     final hasPrefix = startsWith(prefix);
 
     return (
-    prefix: hasPrefix ? substring(0, prefix.length) : '',
-    body: hasPrefix ? substring(prefix.length) : this,
+      prefix: hasPrefix ? substring(0, prefix.length) : '',
+      body: hasPrefix ? substring(prefix.length) : this,
     );
   }
 }
