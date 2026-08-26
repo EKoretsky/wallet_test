b2fe9d59e3c98f96cdba4353077377bf98223cf8
Eugene Koretsky <E.Koretsky@outlook.com>
2026-08-26 14:15:58 +0300

    Map transfer sync errors from HTTP status

--- lib/core/errors/app_exception.dart
+++ lib/core/errors/app_exception.dart
@@ -16,6 +16,29 @@ class TransferSyncException extends AppException {
     required super.code,
     super.message,
   });
+
+  factory TransferSyncException.fromHttpStatus(int? statusCode) {
+    final code = switch (statusCode) {
+      401 => 'unauthorized',
+      404 => 'notFound',
+      409 => 'conflict',
+      408 || 429 => 'rateLimited',
+      503 => 'serverUnavailable',
+      500 => 'internal',
+      null => 'network',
+      _ => 'internal',
+    };
+
+    final message = switch (statusCode) {
+      null => 'Network error',
+      _ => 'HTTP status: $statusCode',
+    };
+
+    return TransferSyncException(
+      code: code,
+      message: message,
+    );
+  }
 }
 
 class CancelException implements Exception {
